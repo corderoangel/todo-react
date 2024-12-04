c3b48a408ee42b12f0af50ca5515d42fa4e39c91
corderoangel <corderoan777@gmail.com>
2024-12-04 17:14:16 -0500

    eventos onClick y onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,7 +2,14 @@ import "./TodoSearch.css";
 
 function TodoSearch(){
     return (
-        <input className="TodoSearch" placeholder="Escribir todo"/>
+        <input className="TodoSearch" placeholder="Escribir todo"
+        onChange={(event) => {
+            console.log("Escribiendo...");
+            console.log(event);
+            console.log(event.target);
+            console.log(event.target.value);                     
+        }}
+        />
     );
 }
 
