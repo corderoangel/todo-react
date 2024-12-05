ada9cadb470e991d7b9764f705478d19b0359d8d
corderoangel <corderoan777@gmail.com>
2024-12-05 08:37:34 -0500

    iconos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,17 @@
+import { CompleteIcon } from "./CompleteIcon";
+import { DeleteIcon } from "./DeleteIcon";
 import "./TodoItem.css";
 
 function TodoItem(props){
     return(
       <li className="TodoItem">
-        <span onClick={props.onComplete}>V</span>
-        <p>{props.text}</p>
-        <span onClick={props.onDelete}>X</span>
+        {/* <span onClick={props.onComplete}>V</span> */}
+        <CompleteIcon completed={props.completed} onComplete={props.onComplete} />
+
+        <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>{props.text}</p>
+        {/* <span onClick={props.onDelete}>X</span> */}
+        <DeleteIcon onDelete={props.onDelete} />
+
       </li>
     );
   };
