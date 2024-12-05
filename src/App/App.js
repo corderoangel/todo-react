4c5682a628c15a257578e787ff80495b63b9696c
corderoangel <corderoan777@gmail.com>
2024-12-05 17:36:45 -0500

    organizar carpetas pt1

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import { CreateTodoButton } from './CreateTodoButton';
-import { TodoCounter } from './TodoCounter';
-import { TodoItem } from './TodoItem';
-import { TodoList } from './TodoList';
-import { TodoSearch } from './TodoSearch';
+import { CreateTodoButton } from '../CreateTodoButton/CreateTodoButton';
+import { TodoCounter } from '../TodoCounter/TodoCounter';
+import { TodoItem } from '../TodoItem/TodoItem';
+import { TodoList } from '../TodoList/TodoList';
+import { TodoSearch } from '../TodoSearch/TodoSearch';
 import React from 'react';
 
 // const defaultTodos = [
