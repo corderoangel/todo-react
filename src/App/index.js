6292f3a294893a30e1276edc016cf412a530a270
corderoangel <corderoan777@gmail.com>
2024-12-07 20:42:14 -0500

    ajustes todo

--- src/App/index.js
+++ src/App/index.js
@@ -2,15 +2,6 @@ import React from 'react';
 import { AppUI } from './AppUI';
 import { TodoProvider } from '../TodoContext';
 
-// const defaultTodos = [
-// 	{ text: "Aprender react", completed: false },
-// 	{ text: "Aprender vite", completed: false },
-// 	{ text: "Aprender next.js", completed: false },
-// 	{ text: "Aprender Astro", completed: false },
-// ];
-// localStorage.setItem("TODOS_V1", JSON.stringify(defaultTodos));
-// localStorage.removeItem("TODOS_V1");
-
 function App() {    
 
   return (
